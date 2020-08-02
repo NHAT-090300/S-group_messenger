8006916d580ee468a55d7f24fdc05d846f903ec2
nhatnguyen <nhatnguyen.090300@gmail.com>
2020-08-02 20:49:18 +0700

    delete all files

--- src/database/firebase-admin.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../database/serviceAccount.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://messenger-app-778ba.firebaseio.com',
-});
-
-export default admin;
